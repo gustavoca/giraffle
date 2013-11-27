32f7c48d5037343510e90ac42b13a73cc1f25d7a
Pablo Crivella <pablocrivella@gmail.com>
2013-11-27 10:32:11 -0400

    add better_errors.rb to initializers folder

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,9 +1,6 @@
 Giraffle::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   
-  # Allow usage on Vagrant
-  BetterErrors::Middleware.allow_ip! "10.0.2.2" if defined? BetterErrors && Rails.env == :development
-  
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
---
+++ config/initializers/better_errors.rb
@@ -0,0 +1,3 @@
+# Allow usage on Vagrant
+BetterErrors::Middleware.allow_ip! "10.0.2.2" if defined? BetterErrors && Rails.env == :development
+BetterErrors.editor = :sublime if defined? BetterErrors
