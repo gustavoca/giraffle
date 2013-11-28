0ed2575bf80e9e5b990bd2f433ccb5a817217f13
Pablo Crivella <pablocrivella@gmail.com>
2013-11-28 14:08:18 -0400

    compile assets for heroku

--- config/initializers/better_errors.rb
+++ config/initializers/better_errors.rb
@@ -1,3 +1,5 @@
 # Allow usage on Vagrant
-BetterErrors::Middleware.allow_ip! "10.0.2.2" if defined? BetterErrors && Rails.env == :development
-BetterErrors.editor = :sublime if defined? BetterErrors
+if defined? BetterErrors
+	BetterErrors::Middleware.allow_ip! "10.0.2.2" if Rails.env == :development
+	BetterErrors.editor = :sublime
+end
